2f2a108b0236f2f7a03ba5bcd44b89abe86877f5
varunamachi <varunamachi@gmail.com>
2020-02-10 21:31:08 +0530

    UI for recent stats

--- src/app/kpx/app-stats/app-stats.component.ts
+++ src/app/kpx/app-stats/app-stats.component.ts
@@ -1,9 +1,15 @@
 import {Component, OnInit} from '@angular/core';
+import {SelectItem} from 'primeng/primeng';
 
 import {StatType} from '../../basic/basic.model';
 import {MsgService} from '../../basic/msg.service';
 import {RecentStat} from '../kpx.model';
 import {KpxService} from '../kpx.service';
+
+interface Info {
+  name: string;
+  id: string;
+}
 
 @Component({
   selector: 'app-app-stats',
@@ -11,6 +17,29 @@ import {KpxService} from '../kpx.service';
   styleUrls: ['./app-stats.component.css']
 })
 export class AppStatsComponent implements OnInit {
+  RECENT_INFO: Info[] = [
+    {name: 'Devices', id: 'devices'},
+    {name: 'Launches', id: 'launches'},
+    {name: 'New Users', id: 'newUsers'},
+    {name: 'Errors', id: 'errors'},
+  ];
+
+
+
+  CHARTS: SelectItem[] = [
+    {label: 'Devices', value: 'devices'},
+    {label: 'Launches', value: 'launches'},
+    {label: 'New Users', value: 'newUsers'},
+    {label: 'Errors', value: 'errors'},
+  ];
+
+  DAYS: SelectItem[] = [
+    {label: '15 Days', value: 15},
+    {label: '30 Days', value: 30},
+    {label: '60 Days', value: 60},
+    {label: '90 Days', value: 90},
+  ];
+
   stats: RecentStat;
 
   constructor(private kpxSrv: KpxService, private msgSrv: MsgService) {}
@@ -27,8 +56,13 @@ export class AppStatsComponent implements OnInit {
   }
 
   load() {
-    this.kpxSrv.getRecentStats().subscribe((stats: RecentStat) => {
-      this.stats = stats;
-    }, err => {this.msgSrv.showError('Failed to fetch stats from server')});
+    this.kpxSrv.getRecentStats().subscribe(
+        (stats: RecentStat) => {
+          this.stats = stats;
+        },
+        err => {this.msgSrv.showError('Failed to fetch stats from server')},
+    );
   }
+
+  loadStats(id: string) {}
 }
--- src/app/prmng/prmng.module.ts
+++ src/app/prmng/prmng.module.ts
@@ -1,109 +1,42 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {
-    ButtonModule,
-    GrowlModule,
-    MessagesModule,
-    MessageModule,
-    MenuModule,
-    PanelMenuModule,
-    InputTextModule,
-    CalendarModule,
-    DropdownModule,
-    DataTableModule,
-    DialogModule,
-    ConfirmDialogModule,
-    PanelModule,
-    CheckboxModule,
-    ListboxModule,
-    MultiSelectModule,
-    InputSwitchModule,
-    SelectButtonModule,
-    PaginatorModule,
-    TriStateCheckboxModule,
-    TreeModule,
-    ToggleButtonModule,
-    ChipsModule,
-    TooltipModule,
-    InputMaskModule,
-    SpinnerModule,
-    ChartModule,
-    TabViewModule,
-
-} from 'primeng/primeng'
-import {
-    TableModule,
-} from 'primeng/table'
-import { ConfirmationService } from 'primeng/components/common/confirmationservice';
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {ConfirmationService} from 'primeng/components/common/confirmationservice';
+import {ButtonModule, CalendarModule, CardModule, ChartModule, CheckboxModule, ChipsModule, ConfirmDialogModule, DataTableModule, DialogModule, DropdownModule, GrowlModule, InputMaskModule, InputSwitchModule, InputTextModule, ListboxModule, MenuModule, MessageModule, MessagesModule, MultiSelectModule, PaginatorModule, PanelMenuModule, PanelModule, SelectButtonModule, SpinnerModule, TabViewModule, ToggleButtonModule, TooltipModule, TreeModule, TriStateCheckboxModule} from 'primeng/primeng';
+import {TableModule} from 'primeng/table';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        ButtonModule,
-        GrowlModule,
-        MessagesModule,
-        MessageModule,
-        MenuModule,
-        PanelMenuModule,
-        InputTextModule,
-        CalendarModule,
-        DropdownModule,
-        DataTableModule,
-        DialogModule,
-        ConfirmDialogModule,
-        PanelModule,
-        CheckboxModule,
-        ListboxModule,
-        MultiSelectModule,
-        InputSwitchModule,
-        SelectButtonModule,
-        PaginatorModule,
-        TriStateCheckboxModule,
-        ToggleButtonModule,
-        ChipsModule,
-        TableModule,
-        TooltipModule,
-        TreeModule,
-        InputMaskModule,
-        SpinnerModule,
-        ChartModule,
-        TabViewModule,
-        SelectButtonModule,
-    ],
-    exports: [
-        ButtonModule,
-        GrowlModule,
-        MessagesModule,
-        MessageModule,
-        MenuModule,
-        PanelMenuModule,
-        InputTextModule,
-        CalendarModule,
-        DropdownModule,
-        DataTableModule,
-        DialogModule,
-        ConfirmDialogModule,
-        PanelModule,
-        CheckboxModule,
-        ListboxModule,
-        MultiSelectModule,
-        InputSwitchModule,
-        SelectButtonModule,
-        PaginatorModule,
-        TriStateCheckboxModule,
-        ToggleButtonModule,
-        ChipsModule,
-        TableModule,
-        TooltipModule,
-        TreeModule,
-        InputMaskModule,
-        SpinnerModule,
-        ChartModule,
-        TabViewModule,
-        SelectButtonModule,
-    ],
-    providers: [
-        ConfirmationService,
-    ]
+  imports: [
+    CommonModule,        ButtonModule,      GrowlModule,
+    MessagesModule,      MessageModule,     MenuModule,
+    PanelMenuModule,     InputTextModule,   CalendarModule,
+    DropdownModule,      DataTableModule,   DialogModule,
+    ConfirmDialogModule, PanelModule,       CheckboxModule,
+    ListboxModule,       MultiSelectModule, InputSwitchModule,
+    SelectButtonModule,  PaginatorModule,   TriStateCheckboxModule,
+    ToggleButtonModule,  ChipsModule,       TableModule,
+    TooltipModule,       TreeModule,        InputMaskModule,
+    SpinnerModule,       ChartModule,       TabViewModule,
+    SelectButtonModule,  CardModule
+  ],
+  exports: [
+    ButtonModule,       GrowlModule,
+    MessagesModule,     MessageModule,
+    MenuModule,         PanelMenuModule,
+    InputTextModule,    CalendarModule,
+    DropdownModule,     DataTableModule,
+    DialogModule,       ConfirmDialogModule,
+    PanelModule,        CheckboxModule,
+    ListboxModule,      MultiSelectModule,
+    InputSwitchModule,  SelectButtonModule,
+    PaginatorModule,    TriStateCheckboxModule,
+    ToggleButtonModule, ChipsModule,
+    TableModule,        TooltipModule,
+    TreeModule,         InputMaskModule,
+    SpinnerModule,      ChartModule,
+    TabViewModule,      SelectButtonModule,
+    CardModule
+  ],
+  providers: [ConfirmationService]
 })
-export class PrmngModule { }
+export class PrmngModule {
+}
